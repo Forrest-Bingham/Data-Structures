ad0edb8e47aba6c96a357d2d592a890691818b43
Forrest Bingham <bingham_5@live.com>
2020-04-22 16:04:47 -0600

    Finished MVP Day 1 Binary Tree

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import ListNode
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +10,12 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.storage = {}
+        self.size = 0
+        self.ordering = DoublyLinkedList()
+
+        #[1]-[2]-[3]-[4]-[5]    [d]-[z]-[c]-[a]
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,17 +25,63 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
-
-    """
-    Adds the given key-value pair to the cache. The newly-
-    added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
-    before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
-    want to overwrite the old value associated with the key with
-    the newly-specified value.
-    """
+        #Push it to the tail since it is most recently used
+        print(key)
+        #Check to see if key exists:
+        if key in self.storage:
+            #Move key to to end so it is most recent
+            print("Checking to see if key is in storage...")
+            node = self.storage[key]
+            print(node.value, "---Node")
+            self.ordering.move_to_end(node)
+            #value[0] is the key, value[1] is the value
+            return node.value[1]
+        #If no key exists
+        else:
+            return None
+        """
+        Adds the given key-value pair to the cache. The newly-
+        added pair should be considered the most-recently used
+        entry in the cache. If the cache is already at max capacity
+        before this entry is added, then the oldest entry in the
+        cache needs to be removed to make room. Additionally, in the
+        case that the key already exists in the cache, we simply
+        want to overwrite the old value associated with the key with
+        the newly-specified value.
+        """
     def set(self, key, value):
-        pass
+        
+        #Adding an item puts it in the back since it is the most used
+        #if key value pair exists, replace it with this current value
+        if key in self.storage:
+            node = self.storage[key]
+            #Overwrite old value
+            node.value = (key, value)
+            #move this node to the tail
+            self.ordering.move_to_end(node)
+            return
+        
+        #If stack is at max capacity, erase least used and replace it with new item
+        if self.size == self.limit:
+            #get rid of least recently used element
+            oldest_key = self.ordering.head.value[0]
+            del self.storage[oldest_key]
+            #Remove head node from the DLL
+            self.ordering.remove_from_head()
+            self.size -= 1
+
+        #KEy is not in storage and still have room in our cache
+        #Add the key and value
+        self.ordering.add_to_tail((key, value))
+        self.storage[key] = self.ordering.tail
+        self.size += 1
+
+#  def test_cache_overwrite_appropriately(self):
+#         self.cache.set('item1', 'a')
+#         self.cache.set('item2', 'b')
+#         self.cache.set('item3', 'c')
+
+#         self.cache.set('item2', 'z')
+
+#         self.assertEqual(self.cache.get('item1'), 'a')
+#         self.assertEqual(self.cache.get('item2'), 'z')
---
+++ queue_and_stack/codechallenge.py
@@ -0,0 +1,23 @@
+# Print out all of the numbers in the following array that are divisible by 3:
+# [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+# The expected output for the above input is:
+# 27
+# 81
+# 9
+# 27
+# 99
+# 63
+# 42
+# You may use whatever programming language you wish.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework 
+# while going through your thought process.
+
+array = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+
+def divide(arr):
+    for x in range(0,len(arr)):
+        #
+        if arr[x] % 3 == 0:
+            print(arr[x])
+
+divide(array)
