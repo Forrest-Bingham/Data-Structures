7ad71ab651fff2257cf414a7913b254aa864340f
Forrest Bingham <bingham_5@live.com>
2020-04-23 17:22:45 -0600

    Finished BST Day 2

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -93,22 +93,87 @@ class BinarySearchTree:
         #if there is a value to the left, do the cb function
         if self.left:
             self.left.for_each(cb)
+
+    def iterative_for_each(self, cb):
+        stack = []
+
+        stack.append(self)
+
+    #loop as long as stack has elements
+        while len(stack) > 0:
+            current_node = stack.pop()
+
+        #check if right child exists
+            if current_node.right:
+                stack.append(current_node.right)
+        #check if left child exists
+            if current_node.left:
+                stack.append(current_node.left)
+        
+        cb(current_node.value)
+
+    def breadth_first_iterative_for_each(self, cb): #Fifo
+        #depth first : stack LIFO
+        #breadth-first: Queue FIFO
+        q = deque()
+        q.append(self)
+
+        while len(q)>0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            cb(self.value)
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
+    # Hint:  Use a recursive, depth first traversal (starting at root and going down to leaves)
     def in_order_print(self, node):
-        pass
+        if self.left:
+            self.left.in_order_print(self.left)
+
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(self.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+    # Rule 1 − Visit the adjacent unvisited vertex. Mark it as visited. Display it. Insert it in a queue.
+
+    # Rule 2 − If no adjacent vertex is found, remove the first vertex from the queue.
+
+    # Rule 3 − Repeat Rule 1 and Rule 2 until the queue is empty.
     def bft_print(self, node):
-        pass
+        q = Queue()
+        q.enqueue(node)
+
+        while q.len() > 0: #Keep running until the queue has nothing in it.
+            current_node = q.dequeue()
+            print(current_node.value)
+
+            if current_node.left:
+                q.enqueue(current_node.left)
+
+            if current_node.right is not None:
+                q.enqueue(current_node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        s = Stack()
+        s.push(node)
+
+        while s.len() > 0:
+            current_node = s.pop()
+            print(current_node.value)
+
+            if current_node.left:
+                s.push(current_node.left)
+
+            if current_node.right:
+                s.push(current_node.right)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
